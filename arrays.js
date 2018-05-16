5de5d800fc69b6928f9f9a977e52e2b06f18cea7
Wendy She <whshiwen@gmail.com>
2018-05-16 20:01:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,3 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
-function destructivelyAddElementToBeginningOfArray(Array,itemToAdd){return(Array.unshift(`${itemToAdd}`))}
+function destructivelyAddElementToBeginningOfArray(Array,elemnet){return(Array.unshift(`${element}`))}
 function addElementToBeginningOfArray(array,element){var newArray=[`${element}`,...array];return newArray}
