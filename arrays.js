a992b6e9d2bef3f515352142fadb06fb6231824e
Wendy She <whshiwen@gmail.com>
2018-05-16 20:15:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,4 @@ function addElementToBeginningOfArray(array,element){var newArray=[`${element}`,
 function addElementToEndOfArray(array,element){var NewArray=[...array,`${element}`];return(NewArray)}
 function destructivelyAddElementToEndOfArray(array,element){array.push(`${element}`);return(array)}
 
-function accessElementInArray(array,index)
+function accessElementInArray(array,index){return(array[index])}
